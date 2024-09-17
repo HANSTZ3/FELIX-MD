5ab4f150f6cc90ac8f9a12359238edf9ab968758
HANSTZ3/HANS-MD <mpyabottzhans@gmail.com>
2024-09-17 16:57:51 -0700

    Add files via upload

---
+++ CONTRIBUTING.md
@@ -0,0 +1,2 @@
+How To Contribute To Us Create an Issue on Github Add you phone number or email and tell us to Contact You Then Us how and why you want to contribute to this project FELIX MD You can send anything via VODACOM Mpesa number 0756530143 
+MARIAN BILALY JUMNE
---
+++ set.js
@@ -0,0 +1,38 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || '',
+    PREFIXE: process.env.PREFIX || ".",
+    OWNER_NAME: process.env.OWNER_NAME || "hansTech",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "hans",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
+    BOT : process.env.BOT_NAME || 'LUCKY_',
+    URL : process.env.BOT_MENU_LINKS || 'https://i.imgur.com/RlQesRp.jpeg,https://i.imgur.com/liFs0Jm.jpeg.https://i.imgur.com/sHK0XEP.jpeg',
+    MODE: process.env.PUBLIC_MODE || "yes",
+    PM_PERMIT: process.env.PM_PERMIT || 'yes',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ETAT : process.env.PRESENCE || '',
+    CHATBOT : process.env.PM_CHATBOT || 'no',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+   
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise  jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
